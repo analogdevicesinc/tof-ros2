1bcd87f25fdd04a87bf9593232631895f82ef810
rbudai <robert.budai@analog.com>
2022-11-30 16:28:19 +0200

    Final docker file version for ubuntu 20 and 22 on ROS2
    Signed-off-by: rbudai <robert.budai@analog.com>

---
+++ dockers/docker_build.sh
@@ -0,0 +1,10 @@
+#Building docker files from source
+echo "Building dockers"
+
+#Ubuntu 20
+sudo docker build --tag ubuntu20.04/ros2_foxy ./ubuntu_20_04_foxy/
+sudo docker build --tag ubuntu20.04/ros2_galactic ./ubuntu_20_04_galactic/
+# sudo docker build --tag ubuntu20.04/ros2_humble ./ubuntu_20_04_humble/
+
+#Ubuntu 22
+sudo docker build --tag ubuntu22.04/ros2_humble ./ubuntu_22_04_humble/
