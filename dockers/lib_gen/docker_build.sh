b3e17727823619ecc2c96c3e2d29b438a0f72f4d
rbudai <robert.budai@analog.com>
2024-01-19 13:54:03 +0100

    Docker: Add readme and add user docker files
    Signed-off-by: rbudai <robert.budai@analog.com>

--- dockers/lib_gen/docker_build.sh
+++ dockers/lib_gen/docker_build.sh
@@ -4,7 +4,7 @@ echo "Building dockers"
 #Ubuntu 20
 sudo docker build --tag ubuntu20.04/ros2_foxy ./ubuntu_20_04_foxy/
 sudo docker build --tag ubuntu20.04/ros2_galactic ./ubuntu_20_04_galactic/
-# sudo docker build --tag ubuntu20.04/ros2_humble ./ubuntu_20_04_humble/
+sudo docker build --tag ubuntu20.04/ros2_humble ./ubuntu_20_04_humble/
 
 #Ubuntu 22
 sudo docker build --tag ubuntu22.04/ros2_humble ./ubuntu_22_04_humble/
